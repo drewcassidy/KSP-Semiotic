929b6463677bc9afef3fb75529bef83ced73fe0b
drewcassidy <drewcassidy@me.com>
2018-05-29 10:33:56 -0700

    Switch image files from png to uncompressed dds

--- Source/Split.sh
+++ Source/Split.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 mkdir -p Sprites
+#./solidify.sh Moon.png
+#./solidify.sh Semiotic.png
 
-convert Semiotic.png -crop 128x128 +adjoin Sprites/Semiotic_%02d.png
-convert Moon.png -crop 128x128 +adjoin Sprites/Moon_%02d.png
-./solidify.sh Sprites/*
+convert Semiotic.png -define dds:compression=none -crop 128x128 +adjoin Sprites/Semiotic_%02d.dds
+convert Moon.png -define dds:compression=none -crop 128x128 +adjoin Sprites/Moon_%02d.dds
+
+cp Sprites/Moon_{0{0..9},{10..15}}.dds ../Assets/
+cp Sprites/Semiotic_{0{0..9},{10..35}}.dds ../Assets
 
